cb957d82aa624cf61dc88d1d254cb0803facd55c
gran1um <65013672+gran1um@users.noreply.github.com>
2021-11-08 22:44:31 +0300

    1.1

--- src/Home.js
+++ src/Home.js
@@ -34,17 +34,15 @@ export default function Home() {
                       <a href="tel:+81111111111">+ 7 111 111 11 11</a>
                     </li>
                     <li>
-                      <a href="mailto:vladilena@yandex.ru">
-                        vladilena@yandex.ru
-                      </a>
+                      <a href="mailto:vladilena@yandex.ru">mail@yandex.ru</a>
                     </li>
                     <li>
-                      <a href="callto:vladilena">Skype: vladilena</a>
+                      <a href="callto:vladilena">Skype: another_user</a>
                     </li>
                   </ul>
                   <div class="address">
                     <p style={{ textAlign: "center" }}>
-                      Высоковольтная 34, Рязань
+                      Высоковольтная 34, Москва
                     </p>
                   </div>
                 </div>
@@ -69,11 +67,9 @@ export default function Home() {
                   <div class="section">
                     <h2>Образование</h2>
                     <ul>
-                      <li>
-                        2006-2012, Спасская детская школа искусств, гитара
-                      </li>
-                      <li>2007-2018, Спасская Гимназия</li>
-                      <li>2018-2023, РГРТУ, Экономист</li>
+                      <li>20XX-20XX, Детская школа искусств, гитара</li>
+                      <li>20XX-20XX, Гимназия</li>
+                      <li>20XX-20XX, МГУ, Экономист</li>
                     </ul>
                   </div>
                 </div>
@@ -82,12 +78,12 @@ export default function Home() {
                     <h2>Достижения</h2>
                     <ul>
                       <li>
-                        Призер олимпиады по литературе среди 5 классов: 2012
+                        Призер олимпиады по литературе среди 5 классов: 20XX
                       </li>
                       <li>
-                        Победитель олимпиады по математике среди 7 классов: 2014
+                        Победитель олимпиады по математике среди 7 классов: 20XX
                       </li>
-                      <li>Призер конференции по экономике: 2020</li>
+                      <li>Призер конференции по экономике: 20XX</li>
                     </ul>
                   </div>
                 </div>
@@ -97,17 +93,26 @@ export default function Home() {
               <article>
                 <header>
                   <h1 class="text-center cv-header mt-5 mb-4">
-                    Владилена Александровна{" "}
-                    <span class="color-grey">Останина</span>
+                    Александра Александровна{" "}
+                    <span class="color-grey">Александрова</span>
                   </h1>
                   <h2 class="job-title text-center lines color-grey mb-4">
                     Резюме
                   </h2>
                   <h3 class="mb-3 profile color-grey">Обо мне</h3>
                   <p class="mb-5">
-                   <li>Уверенный пользователь ПК, владею следующими программами: Microsoft Word, Microsoft Excel,  Microsoft Power Point, Microsoft Publisher, 1С Бухгалетрия 7 и 8, Клиент-Банк.</li>
-                   <li>Опыт работы более трех лет в бухгалтерской сфере, один из которых в бюджетной организации. Обладаю знаниями в области бухгалтерского, налогового учета, финансового анализа и бюджетирования.</li> 
-                    </p>
+                    <li>
+                      Уверенный пользователь ПК, владею следующими программами:
+                      Microsoft Word, Microsoft Excel, Microsoft Power Point,
+                      Microsoft Publisher, 1С Бухгалетрия 7 и 8, Клиент-Банк.
+                    </li>
+                    <li>
+                      Опыт работы более трех лет в бухгалтерской сфере, один из
+                      которых в бюджетной организации. Обладаю знаниями в
+                      области бухгалтерского, налогового учета, финансового
+                      анализа и бюджетирования.
+                    </li>
+                  </p>
                   <h3 class="mb-3 expirience color-grey">Опыт работы</h3>
                   <section class="mb-5">
                     <h4 class="company-name mb-2">Альфа-строй, 2019-2019</h4>
